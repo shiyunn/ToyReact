6b61a04698eb459f1bd50066402e145fc48d4fdc
石运 <guzhiyun2001@163.com>
2020-09-08 21:07:36 +0800

    增加rang渲染

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-import {createElement, Component, render} from './toy-react'
+import { createElement, Component, render } from './toy-react'
 class MyComponent extends Component {
-   render(){
-   return <div><h1>my component</h1> {this.children}</div>
-   }
+    constructor() {
+        super()
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
+    render() {
+        return <div>
+            <h1>my component</h1>
+            {this.children}
+            <span>{this.state.a.toString()}</span>
+        </div>
+    }
 }
 
 
 render(<MyComponent id="a" class="c">
+
     <div>1</div>
     <div>2</div>
 </MyComponent>, document.body)
--- toy-react.js
+++ toy-react.js
@@ -1,5 +1,6 @@
+const RENDER_TO_DOM = Symbol('render to dom')
 class ElementWrapper {
-    constructor(type){
+    constructor(type) {
         this.root = document.createElement(type)
     }
 
@@ -8,18 +9,30 @@ class ElementWrapper {
     }
 
     appendChild(component) {
-        this.root.appendChild(component.root)
+        let range = document.createRange()
+        range.setStart(this.root, 0)
+        range.setEnd(this.root, this.root.childNodes.length)
+        range.deleteContents()
+        component[RENDER_TO_DOM](range)
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertCode(this.root)
     }
 }
 
 class TextWraper {
-    constructor(content){
+    constructor(content) {
         this.root = document.createTextNode(content)
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertCode(this.root)
     }
 }
 
 export class Component {
-    constructor(type){
+    constructor(type) {
         this.props = Object.create(null)
         this.children = []
         this._root = null
@@ -33,25 +46,26 @@ export class Component {
         this.children.push(component)
     }
 
-    get root() {
-        if(!this._root) {
-            this._root = this.render().root
-        }
-        return this._root
+    [RENDER_TO_DOM](range) {
+        this.render()[RENDER_TO_DOM](range)
     }
 }
 
 
-export function render(component,  parentElement){
-    parentElement.appendChild(component.root)
+export function render(component, parentElement) {
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
 }
 
 
 
 export function createElement(type, attributes, ...children) {
     let e;
-    if(typeof type === 'string') {
-        e =  new ElementWrapper(type)
+    if (typeof type === 'string') {
+        e = new ElementWrapper(type)
     } else {
         e = new type
     }
@@ -59,21 +73,21 @@ export function createElement(type, attributes, ...children) {
         e.setAttribute(p, attributes[p])
     }
     let insertChildren = (children) => {
-        for(let child of children) {
-            if(typeof child === 'string') {
+        for (let child of children) {
+            if (typeof child === 'string') {
                 child = new TextWraper(child)
             }
-            if(typeof child === 'object' && child instanceof Array) {
+            if (typeof child === 'object' && child instanceof Array) {
                 insertChildren(child)
-            }else {
+            } else {
                 e.appendChild(child)
             }
-           
+
         }
     }
 
     insertChildren(children)
-    
+
 
     return e
 }
